4b89ad997f8cdbe8ea94f2a102e9b106b28c7af1
Dididididid1501 <vasiliagleev@gmail.com>
2026-04-09 09:27:35 +0500

    еще меняем

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 const SUPABASE_URL = 'https://dwfcbkqtmjdrjxajhqov.supabase.co'; // Замените на свой Project URL
-const SUPABASE_ANON_KEY = 'sb_publishable_dnVYGKhSglDxpTjlaJtpRQ_3xRvgzCD'; // Замените на свой anon public key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3ZmNia3F0bWpkcmp4YWpocW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NDE3MTUsImV4cCI6MjA5MTIxNzcxNX0.I3Y4QZQG_0JY3fN1d_ZHrwG1Gm--BtXz--JBFy8b4aA'; // Замените на свой anon public key
